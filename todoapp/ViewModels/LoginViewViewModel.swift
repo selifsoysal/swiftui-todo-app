5e592e17d5b398e13bfa13501515affc025f595a
Elif S. <s.elifsoysal@icloud.com>
2025-04-21 23:10:20 +0300

    Login sayfasinin validation islemlerini yapar.

--- todoapp/ViewModels/LoginViewViewModel.swift
+++ todoapp/ViewModels/LoginViewViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class LoginViewViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    
+    init(){}
+    
+    func login(){
+        guard validate()
+        else {
+            return
+        }
+        
+    }
+    
+    func validate() -> Bool{
+        errorMessage = ""
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty
+        else {
+            errorMessage = "Lütfen tüm alanları doldurun!"
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".")
+        else {
+            errorMessage = "Lütfen geçerli bir email adresi girin!"
+            return false
+        }
+        
+        return true
+    }
+}
