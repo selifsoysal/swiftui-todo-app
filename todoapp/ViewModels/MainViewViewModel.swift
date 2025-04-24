f19e6e1f66804caaa19c625b48cc627c5464851e
Elif S. <s.elifsoysal@icloud.com>
2025-04-24 22:45:42 +0300

    MainViewViewModel.swift dosyasinin kodlarini yukler.

--- todoapp/ViewModels/MainViewViewModel.swift
+++ todoapp/ViewModels/MainViewViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by S. Elif Soysal on 14.04.2025.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel:ObservableObject {
+    @Published var currentUserId: String = ""
+    
+    init() {
+        Auth.auth().addStateDidChangeListener{ [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
