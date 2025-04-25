51c07091dff5f29c06996d65d1c1f3359f86ecc4
Elif S. <s.elifsoysal@icloud.com>
2025-04-25 22:42:45 +0300

    RegisterViewViewModel.swift dosyasinin kodlarini yukler.

--- todoapp/ViewModels/RegisterViewViewModel.swift
+++ todoapp/ViewModels/RegisterViewViewModel.swift
@@ -5,4 +5,66 @@
 //  Created by S. Elif Soysal on 14.04.2025.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
+
+class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+
+    
+    init(){}
+    
+    func register(){
+        guard validate() else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password){
+            [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            //insert metodu
+        }
+    }
+    
+    private func insertUserRecord(id: String){
+        let newUser = User(id: id, name: name, email: email, joined: Date().timeIntervalSince1970)
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(id)
+            .setData(newUser.asDictionary())
+    }
+    
+    private func validate() -> Bool {
+        errorMessage = ""
+        
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty
+        else {
+            errorMessage = "Lütfen tüm alanları doldurunuz!"
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".")
+        else {
+            errorMessage = "Geçerli bir email adresi giriniz!"
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            errorMessage = "Şifreniz en az 6 karakterden oluşmalıdır!"
+            return false
+        }
+        
+        return true
+    }
+    
+    
+}
